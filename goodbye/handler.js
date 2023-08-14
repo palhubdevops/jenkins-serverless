ebe873d5a7da81409728de4294248c66cb943224
palhubdevops <141798474+palhubdevops@users.noreply.github.com>
2023-08-14 15:02:54 +0530

    Update handler.js

--- goodbye/handler.js
+++ goodbye/handler.js
@@ -7,6 +7,7 @@ module.exports.goodbye = async (event) => {
       message: 'Goodbye !',
     }),
   };
+  
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
